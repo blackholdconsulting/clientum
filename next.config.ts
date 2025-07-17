a046b5ba465b496ea7bff2ab9a1a221de47e0106
blackholdconsulting <info@blackholdconsulting.com>
2025-07-17 19:43:36 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,10 @@ const nextConfig = {
       cldr: require.resolve('cldrjs'),
       'cldr/event': require.resolve('cldrjs'),
       globalize: require.resolve('globalize'),
-      'globalize/dist/globalize/event': require.resolve('globalize/dist/globalize/event')
+      'globalize/dist/globalize/event': require.resolve('globalize/dist/globalize/event'),
     };
     return config;
-  }
+  },
 };
+
 module.exports = nextConfig;
