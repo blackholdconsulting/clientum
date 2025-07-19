7826ca8e8825d5ff63540ff2c4e1b575ffbda33a
blackholdconsulting <info@blackholdconsulting.com>
2025-07-19 20:16:31 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 // next.config.js
-/** @type {import('next').NextConfig} */
 const path = require('path')
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
     config.resolve.alias = {
+      // conserva los alias que ya tengas
       ...(config.resolve.alias || {}),
-      // Alias de cldr que globalize/strong-soap esperan
-      cldr: require.resolve('cldrjs'),
+
+      // alias para que globalize / strong-soap encuentren cldr/event y cldr/supplemental
+      'cldr': require.resolve('cldrjs/dist/cldr.js'),
       'cldr/event': require.resolve('cldrjs/dist/cldr/event.js'),
       'cldr/supplemental': require.resolve('cldrjs/dist/cldr/supplemental.js'),
     }
