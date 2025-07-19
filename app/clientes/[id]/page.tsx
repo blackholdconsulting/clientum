970579097e6d1b09efc45a34bab38065da4cb439
blackholdconsulting <info@blackholdconsulting.com>
2025-07-19 18:04:47 +0200

    Update page.tsx

--- app/clientes/[id]/page.tsx
+++ app/clientes/[id]/page.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import ClientePage from "./ClientePage";
 
-export default function Page(props) {
-  // props.params.id existe de todos modos
-  return <ClientePage id={props.params.id} />;
+export default function Page({ params }: { params: { id: string } }) {
+  return <ClientePage id={params.id} />;
 }
