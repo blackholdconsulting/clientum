78cd9265f44270702110bbf96e08f12f7c656502
blackholdconsulting <info@blackholdconsulting.com>
2025-07-24 18:38:02 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,19 @@
 // next.config.js
-const { NodePolyfillPlugin } = require("node-polyfill-webpack-plugin");
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...(config.resolve.fallback || {}),
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      assert: require.resolve("assert/"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      os: require.resolve("os-browserify/browser"),
-      url: require.resolve("url/")
-    };
-    config.plugins.push(new NodePolyfillPlugin());
+const { NodePolyfillPlugin } = require('node-polyfill-webpack-plugin');
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Fallbacks para que crypto y stream funcionen en el bundle del navegador
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+      };
+      // Añadimos el plugin
+      config.plugins.push(new NodePolyfillPlugin());
+    }
     return config;
   },
 };
-
-module.exports = nextConfig;
