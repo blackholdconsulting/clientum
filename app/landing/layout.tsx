1e04af8fa487faca8853fbf12f37f34be2308e65
blackholdconsulting <info@blackholdconsulting.com>
2025-07-29 19:51:32 +0200

    Update layout.tsx

--- app/landing/layout.tsx
+++ app/landing/layout.tsx
@@ -1,10 +1,18 @@
 // app/landing/layout.tsx
 import React from "react";
 
+export const metadata = {
+  title: "Clientum - Landing",
+};
+
 export default function LandingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
 }
