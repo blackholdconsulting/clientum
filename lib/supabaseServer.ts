84459f72afaf6d698d0d307f56943890f867ce36
blackholdconsulting <info@blackholdconsulting.com>
2025-08-02 00:25:40 +0200

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -3,13 +3,11 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!, // usa la key de servicio en el servidor
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
   {
     auth: {
       persistSession: false,
       autoRefreshToken: false,
     },
-    // opcionalmente: define el runtime si haces edge functions
-    // runtime: "edge",
   }
 );
