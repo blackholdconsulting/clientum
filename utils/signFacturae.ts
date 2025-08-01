9f1840e8e1a78c3d496764a866ee9f595ac4de93
blackholdconsulting <info@blackholdconsulting.com>
2025-08-01 14:12:00 +0200

    Update signFacturae.ts

--- utils/signFacturae.ts
+++ utils/signFacturae.ts
@@ -5,8 +5,10 @@ import { SignedXml } from "xml-crypto";
 export function signFacturaeXML(xml: string, p12Buffer: Buffer, password: string): string {
   const p12Asn1 = forge.asn1.fromDer(p12Buffer.toString("binary"));
   const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, password);
+
   const keyObj = p12.getBags({ bagType: forge.pki.oids.pkcs8ShroudedKeyBag })[forge.pki.oids.pkcs8ShroudedKeyBag][0];
   const certObj = p12.getBags({ bagType: forge.pki.oids.certBag })[forge.pki.oids.certBag][0];
+
   const privateKey = forge.pki.privateKeyToPem(keyObj.key);
   const certificate = forge.pki.certificateToPem(certObj.cert);
 
@@ -17,5 +19,6 @@ export function signFacturaeXML(xml: string, p12Buffer: Buffer, password: string
     getKeyInfo: () => `<X509Data><X509Certificate>${certificate}</X509Certificate></X509Data>`,
   };
   sig.computeSignature(xml);
+
   return sig.getSignedXml();
 }
