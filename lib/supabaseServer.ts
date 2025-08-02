5b75b51cda3a40e61a56d072f3b4f552a1ba0019
blackholdconsulting <info@blackholdconsulting.com>
2025-08-02 22:34:21 +0200

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,17 +1,18 @@
 // lib/supabaseServer.ts
 
-import { createServerClient } from "@supabase/auth-helpers-nextjs";
-import { headers, cookies } from "next/headers";
+import { createClient } from "@supabase/supabase-js";
 
-// Usa la URL pública y la Service Role Key o la anon si no existe
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+// Usa la URL de Supabase (service) o la pública si falta
+const supabaseUrl = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL!;
+
+// Usa la service role key o la anon pública si no existe
 const supabaseKey =
   process.env.SUPABASE_SERVICE_ROLE_KEY! ||
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabaseServer = createServerClient({
-  supabaseUrl,
-  supabaseKey,
-  headers,
-  cookies,
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+  },
 });
