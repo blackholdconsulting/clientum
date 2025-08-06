f137cd976435d9de57a30911c762460aebfbe515
blackholdconsulting <info@blackholdconsulting.com>
2025-08-06 18:42:59 +0200

    Update route.ts

--- app/api/clientes/route.ts
+++ app/api/clientes/route.ts
@@ -1,10 +1,11 @@
 // app/api/clientes/route.ts
 import { NextResponse } from 'next/server'
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
 export async function GET() {
-  const supabase = createServerComponentSupabaseClient({ cookies })
+  const supabase = createServerComponentClient({ cookies })
+
   const { data, error } = await supabase
     .from('clientes')
     .select('id,nombre,direccion,cif,cp,email')
